e8ab125e31a3141a2284aa27afa293718475247a
Sulakshan Chandra Ghimire <sulucghimire@gmail.com>
2026-07-09 12:43:08 +0545

    Refactor input.c and update lexer files
    - Modified input.c to change the value of variable 'b' and simplified the return statement.
    - Updated lex2.l and lex3.l to remove unnecessary comments for better readability.
    - Noted binary differences in lex1.exe and lex3.exe.

--- compilerdesign/input.c
+++ compilerdesign/input.c
@@ -1,13 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int a = 10;
-    float b = 25.5;
-    int marks = 100;
-
-    if(a > 5)
-    {
-        return marks;
-    }
+    float b = 3.14;
+    return a;
 }
